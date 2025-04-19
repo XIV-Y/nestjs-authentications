21b485faa60ec42f215aea0ea8ceef30bd1c0b04
XIV-Y <yutodazepa.g@gmail.com>
2025-04-19 18:02:06 +0900

    Fix: JWT認証のコメントアウトとCookie認証ガードの追加

--- backend/src/modules/users/users.controller.ts
+++ backend/src/modules/users/users.controller.ts
@@ -1,10 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Controller, Get, UseGuards, Request } from '@nestjs/common';
 
 import { JwtAuthGuard } from '@/src/modules/jwt/jwt.guard';
+import { CookieBasedGuard } from '@/src/modules/cookie-based/cookie-based.guard';
 
 @Controller('users')
 export class UsersController {
-  @UseGuards(JwtAuthGuard)
+  // JWT認証を使用する場合
+  // @UseGuards(JwtAuthGuard)
+
+  // Cookie認証を使用する場合
+  @UseGuards(CookieBasedGuard)
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
